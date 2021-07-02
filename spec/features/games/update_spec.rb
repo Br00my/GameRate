ff3d5add7f854aeefa13df523e74e5f73f4b5439
Amir13N <meepopopo22@mail.ru>
2021-07-02 21:46:27 +0600

    Games controller corrections

--- spec/features/games/update_spec.rb
+++ spec/features/games/update_spec.rb
@@ -13,7 +13,7 @@ feature 'User can update list of owned games', "
     visit root_path
     click_on 'Sign in'
 
-    click_on 'Look for new games and update playtimes'
+    click_on 'Look for new games and update playtime'
     new_games = [user.games.count - 1, 0].max
     expect(page).to have_content "#{new_games} new #{'game'.pluralize(new_games)} found."
   end
